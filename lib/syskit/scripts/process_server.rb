6ebaa50448965f636dd83c2dfc6bfc73f9902898
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-09-25 14:47:57 -0300

    app: improve remote process server support
    'syskit process_server' will use LOCAL log dir/results dir information
    to create the log directory, and will save information about itself
    in the (new) log directories' info.yml file, thus allowing us to
    more easily track what each log directory is about.

--- lib/syskit/scripts/process_server.rb
+++ lib/syskit/scripts/process_server.rb
@@ -11,7 +11,30 @@ server_port = Orocos::RemoteProcesses::DEFAULT_PORT
 Roby::Application.host_options(parser, options)
 parser.parse(ARGV)
 
-Orocos::CORBA.name_service.ip = options[:host]
-Orocos::RemoteProcesses::Server.run(Orocos::RemoteProcesses::Server::DEFAULT_OPTIONS, server_port)
+class ProcessServer < Orocos::RemoteProcesses::Server
+    attr_reader :app
+    def initialize(app, port: Orocos::RemoteProcesses::Server::DEFAULT_PORT)
+        @app = app
+        super(wait: false, output: "%m-%p.txt")
+    end
 
+    def create_log_dir(log_dir, time_tag, metadata = Hash.new)
+        if log_dir
+            app.log_base_dir = log_dir
+        end
+        app.add_app_metadata(metadata)
+        app.find_and_create_log_dir(time_tag)
+        Server.debug "  #{app.log_dir} created for #{time_tag}"
+    end
+
+    def start_process(name, deployment_name, name_mappings, options)
+        options = [working_directory: app.log_dir].merge(options)
+        super(name, deployment_name, name_mappings, options)
+    end
+end
+
+Orocos::CORBA.name_service.ip = options[:host]
+Orocos.disable_sigchld_handler = true
+Orocos.initialize
+ProcessServer.new(Roby.app, port: server_port).exec
 
